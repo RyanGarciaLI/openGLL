68004edba9a2e511eb3ba0cd1f9017e6b3abb6bd
RyanLI <lyxnb2333@gmail.com>
2022-10-31 18:32:44 +0800

    LBM XCode update

---
+++ LBMXcode/gpuProj_cmake.build/Debug/gpuProj.build/Objects-normal/x86_64/glad.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/yuxinli/Projects/openGLL/LBMXcode/gpuProj_cmake/glad.c \
+  /Users/yuxinli/Projects/openGLL/LBMXcode/gl_libs/include/glad/glad.h \
+  /Users/yuxinli/Projects/openGLL/LBMXcode/gl_libs/include/KHR/khrplatform.h
